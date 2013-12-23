6b1cf2255a6d8f2735c6b7e5caac4169c85cedb1
Todd Wolfson <todd@twolfson.com>
2013-12-22 23:52:23 -0800

    Added glob search for keymaps

--- what-shortcut.py
+++ what-shortcut.py
@@ -1,8 +1,23 @@
+import glob
+import os
+import sublime
 import sublime_plugin
 
 
 class WhatShortcutCommand(sublime_plugin.WindowCommand):
+    def get_platform_keymaps(self):
+        """Collect all .sublime-keymap files for the current platform
+
+        Example: Packages/User/Default (Linux).sublime-keymap
+        """
+        package_dir = sublime.packages_path()
+        platform = sublime.platform()
+        keymap_glob = os.path.join(package_dir,
+                                   '**/Default ({}).sublime-snippet'.format(platform))
+        keymaps = glob.glob(keymap_glob)
+        return keymaps
+
     def run(self):
         """Be awesome to each other."""
-        view = self.window.active_view()
-        print view.settings().__dict__
+        # view = self.window.active_view()
+        print self.get_platform_keymaps()
